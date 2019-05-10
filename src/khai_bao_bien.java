687c905a0eb06bf8bb069fb575e1bc4059df4d21
minhdien1710 <nguyenminhdien996@gmail.com>
2019-05-10 10:06:02 +0700

    added

---
+++ src/khai_bao_bien.java
@@ -0,0 +1,17 @@
+public class khai_bao_bien {
+    public static void main(String[] arga){
+        int i =10;
+        float f= 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String a = "hanoi";
+
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a);
+    }
+}
